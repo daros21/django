9308d6df1958698dc23206058b6a85f5f194b7c0
daros21 <daros21@op.pl>
2019-11-19 11:03:47 +0100

    adding translations

--- polls/admin.py
+++ polls/admin.py
@@ -2,12 +2,24 @@ from django.contrib import admin
 from .models import Question, Choice
 # Register your models here.
 
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    fields = ['choice_text']
+
 
 class AdminQuestion(admin.ModelAdmin):
     list_display = ['question_text', 'pub_date', 'is_active']
     search_fields = ['question_text']
     list_filter = ['is_active']
 
+    fieldsets = [
+        ('Glowne', {'fields': ['question_text', 'is_active']}),
+        ("daty", {'fields': ['pub_date', 'created', 'modified']}),
+        ("image", {'fields': ['image']}),
+        ("poziom trudnosci", {'fields': ['level']})
+    ]
+    inlines = [ChoiceInLine]
+    readonly_fields = ['id', 'created', 'modified']
 
 class AdminChoice(admin.ModelAdmin):
     pass
--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,11 @@
 import datetime
 
+
 from django.db import models
 
 # Create your models here.
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 LEVEL_CHOICES=[
     ("easy", "easy"),
@@ -13,11 +15,13 @@ LEVEL_CHOICES=[
 
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=300)
-    pub_date = models.DateTimeField(verbose_name="publication date")
-    is_active = models.BooleanField(default=True)
-    level = models.CharField(max_length=200, choices=LEVEL_CHOICES, null=True, blank=False)
-    image = models.ImageField(null=True, blank=True, upload_to="question/images/%Y/%m/%d")
+    question_text = models.CharField(max_length=300, verbose_name=_("question text"))
+    pub_date = models.DateTimeField(verbose_name=_("publication date"))
+    created = models.DateTimeField(auto_now_add=True, verbose_name=_("created"))
+    modified = models.DateTimeField(auto_now=True, verbose_name=_("modified"))
+    is_active = models.BooleanField(default=True, verbose_name=_("is active"))
+    level = models.CharField(max_length=200, choices=LEVEL_CHOICES, null=True, blank=False, verbose_name=_("level"))
+    image = models.ImageField(null=True, blank=True, upload_to="question/images/%Y/%m/%d", verbose_name=_("image"))
 
 
     def was_published_recently(self):
@@ -26,6 +30,11 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    class Meta:
+        verbose_name = verbose_name=_("Question")
+        verbose_name_plural = verbose_name=_("Questions")
+
 
 
 class Choice(models.Model):
