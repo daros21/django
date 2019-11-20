994008a60d8b6244f836498cf52cebd0e41f027a
daros21 <daros21@op.pl>
2019-11-20 12:31:31 +0100

    sending data to excel

--- books/urls.py
+++ books/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from books.views import books_list, book_details, BookList, BookDetail, book_comment
+from books.views import books_list, book_details, BookList, BookDetail, book_comment, books_to_excel
 
 app_name = "books"
 urlpatterns = [
     path("", BookList.as_view(), name="list"),
     path("<int:pk>", book_details, name="details"),
     path("generic/<int:pk>", BookDetail.as_view(), name="details_generic"),
-    path("<int:pk>/comment", book_comment, name='comment')
+    path("<int:pk>/comment", book_comment, name='comment'),
+    path("report/", books_to_excel, name='report'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,16 +1,18 @@
 from django.contrib import messages
 from django.core.paginator import Paginator
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, render_to_response
 
 # Create your views here.
 from django.urls import reverse
+from django.utils import timezone
 from django.views.decorators.http import require_http_methods
 from django.views.generic import ListView, FormView, DetailView
 
 from books.forms import CommentForm
 from books.models import Book
-
+from openpyxl import Workbook
+from openpyxl.styles import Border, Font
 
 
 class BookList(ListView):
@@ -58,3 +60,28 @@ def book_comment(request, pk):
         messages.add_message(request, messages.WARNING, form.errors)
 
     return HttpResponseRedirect(reverse('books:details', args=(pk,)))
+
+def books_to_excel(request):
+    response = HttpResponse(
+        content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    )
+    response['Content-Disposition'] = "attachment; filename=List_{date}.xlsx".format(
+        date=timezone.now().strftime("%Y-%m-%d")
+    )
+    workbook = Workbook()
+    ws = workbook.active
+
+    books = Book.objects.all()
+
+    font = Font(name='Calibri', size = 14, bold = True, italic = False, vertAlign = None, underline = 'none', strike = False, color = 'FF000000')
+
+    ws.append(['tytul', 'strony', 'cena', 'ocena'])
+    for b in books:
+        ws.append([b.title, b.pages, b.price, b.rating])
+
+    ws['A1'].font = font
+    ws['B1'].font = font
+    ws['C1'].font = font
+    ws['D1'].font = font
+    workbook.save(response)
+    return response
